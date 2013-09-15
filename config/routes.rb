a336854d85c1a0b8b7efed755f329d3fb1e92d0e
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-09-15 19:19:47 -0400

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 TeamLunch::Application.routes.draw do
+  get "users/new"
   resources :sessions, only: [:new, :create, :destroy]
   root to: 'static_pages#home'
+  match '/signup', to: 'users#new',         via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/signin', to: 'sessions#new',      via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
