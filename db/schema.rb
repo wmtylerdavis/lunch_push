90c3079b5843f5d167345bca94874f6b334f94f1
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-09-15 20:39:37 -0400

    changing up users a bit with email and password

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130820202646) do
+ActiveRecord::Schema.define(version: 20130916002635) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_token"
+    t.string   "email"
+    t.string   "password_digest"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
