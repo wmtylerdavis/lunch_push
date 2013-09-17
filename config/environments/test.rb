2f105abe2c95b03023b75301f739e0791ccf5b2c
Wm Tyler Davis <williamtylerdavis@gmail.com>
2013-09-16 20:30:51 -0400

    finally got back to sessions after user change

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ TeamLunch::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Speed up tests
+  ActiveModel::SecurePassword.min_cost = true
 end
